bd93a40d8f088accfd9ff4e08f156f23ba1c604c
Jechol Lee <jechol@devall.org>
2025-07-21 16:54:34 +0900

    Temporary ordering

--- lib/transformer.ex
+++ lib/transformer.ex
@@ -11,7 +11,7 @@ defmodule AshMock.Transformer do
   def before?(_), do: false
 
   def after?(Ash.Resource.Transformers.BelongsToAttribute), do: true
-  def after?(AshRankable.Transformer), do: true
+  def after?(Rift.Rankable.Transformer), do: true
   def after?(_), do: false
 
   def transform(dsl_state) do
